d4bce99a22ab3f9e0ae05d3152b2b97dfcbc3a1b
gabrielsantos95 <gabrieldsilvasantos@gmail.com>
2024-05-28 21:50:37 -0300

    alteracao html e css

---
+++ script/main.js
@@ -0,0 +1,8 @@
+const btnNome = document.querySelector("#nome_produto");
+btnNome.addEventListener("click", event => {
+    event.preventDefault();
+})
+
+const inputDoNome = document.querySelector("#nome");
+const valorDoNome = inputDoNome.value;
+
